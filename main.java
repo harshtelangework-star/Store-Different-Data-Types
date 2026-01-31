30c6588a7619686bb15859074397a5911b6e1720
harshtelangework-star <harshtelangework@gmail.com>
2026-01-31 21:45:12 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,20 @@
+//Store Different Data Types
+//Create variables of different types and print them:
+//int (age)
+//double (height)
+//char (grade)
+//boolean (isStudent)
+
+public class main{
+    public static void main(String[]args){
+        int age = 22;
+        double height = 182.88;
+        char grade = 'A';
+        boolean student = true;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(student);
+    }
+}
